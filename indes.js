362f07b44c3d5ca4193cd99796d177566194b523
Brayan Corredor <brayanjcp@outlook.com>
2018-10-30 15:40:26 -0500

    Add functuonality of add selecet

--- indes.js
+++ indes.js
@@ -1,3 +1,9 @@
-exports.printMsg = function() {
-    console.log("This is a message from the demo package");
+exports.module={
+  select:(idSelector,options)=>{
+    var select=document.getElementById(idSelector);
+    options.forEach(function(e){ 
+      var b=document.createElement('option');
+      b.text=e.key;b.value=e.value;
+      document.getElementById('selectCustom').appendChild(b);});
   }
+}
